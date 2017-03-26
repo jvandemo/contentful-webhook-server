c96a0937987328a379a84d7d5cb140c1827031bb
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-03-27 00:14:11 +0200

    feat(examples): add comment on how to use curl to test locally

--- examples/webhook-server.js
+++ examples/webhook-server.js
@@ -1,21 +1,24 @@
+// To test locally with curl, run:
+// $ curl --data "test=data" localhost:3000 --header "x-contentful-topic: ContentManagement.ContentType.publish"
+
 var server = require('../index.js')({
   path: '/',
   username: 'user',
   password: 'pass'
 });
 
-server.on('ContentManagement.error', function(err, req){
+server.on('ContentManagement.error', function (err, req) {
   console.log(err);
 });
 
-server.on('ContentManagement.ContentType.publish', function(req){
+server.on('ContentManagement.ContentType.publish', function (req) {
   console.log('ContentManagement.ContentType.publish');
 });
 
-server.on('ContentManagement.*', function(topic, req){
+server.on('ContentManagement.*', function (topic, req) {
   console.log('*: ' + topic);
 });
 
-server.listen(3000, function(){
+server.listen(3000, function () {
   console.log('Contentful webhook server running on port ' + 3000)
 });
